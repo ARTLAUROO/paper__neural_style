56c6af536eacc84d2496f131ab0cfccf3e70c884
A.L. van Rooijen <s1259008@duranium.liacs.nl>
2017-03-31 08:22:52 +0200

    commit

---
+++ hello_world.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# python neural_style.py --content <content file> --styles <style file> --output <output file>
+python neural_style.py --content images/input/hello_world.jpg --styles images/style/hello_world.jpg --output images/output/hello_world.jpg
